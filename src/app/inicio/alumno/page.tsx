cd9289cde828fd2ed22d64a4e63e1b59f549f020
Francisco Schmidt <franschmidt2415@gmail.com>
2024-09-28 18:51:36 -0300

    Agrega cambios en títulos y links
    Co-authored-by: Franco Feuilles

--- src/app/inicio/alumno/page.tsx
+++ src/app/inicio/alumno/page.tsx
@@ -1,67 +1,8 @@
 
-import { Metadata } from "next"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { Button } from "@/components/ui/button"
-
-const metadata: Metadata = {
-  title: 'Inicio Alumno',
-}
-
-export default async function InicioAlumnoPage() {
+export default function InicioAlumnoPage() {
   return (
-    <>
-      <InicioAlumno />
-    </>
-  );
-}
-
-function InicioAlumno() {
-  const fechaActual = formatearFecha(new Date())
-  return (
-    <div className="h-screen flex flex-col p-5">
-      <div className="flex flex-row justify-between">
-        <div className="flex flex-col items-center">
-          <h2 className="text-3xl font-semibold">Título Página</h2>
-          <h2 className="text-md">{fechaActual}</h2>
-        </div>
-      </div>
-      <div className="flex flex-col mt-auto">
-        <div className="flex flex-row justify-between">
-          <InfoEscolar />
-          <Button variant="outline">Mensajes</Button>
-        </div>
-      </div>
+    <div>
+      
     </div>
-  );
-}
-
-async function InfoEscolar() {
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline">Información Escolar</Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuItem>Amonestaciones</DropdownMenuItem>
-        <DropdownMenuItem>Boletines</DropdownMenuItem>
-        <DropdownMenuItem>Notas Exámenes</DropdownMenuItem>
-        <DropdownMenuItem>Asistencias</DropdownMenuItem>
-        <DropdownMenuItem>Trabajos Prácticos</DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
   )
-}
-
-function formatearFecha(fecha: Date) {
-  const dia = String(fecha.getDate()).padStart(2, '0'); // Asegura que el día tenga dos dígitos
-  const mes = String(fecha.getMonth() + 1).padStart(2, '0'); // Los meses son 0-11, así que se suma 1
-  const año = fecha.getFullYear();
-  // Construye la fecha en el formato dd/mm/aaaa
-  const fechaFormateada = `${dia}/${mes}/${año}`;
-  return fechaFormateada
 }
--- src/app/inicio/padre/page.tsx
+++ src/app/inicio/padre/page.tsx
@@ -1,68 +1,8 @@
-import { Metadata } from "next"
-import HijosDropdown from "@/components/inicio/padre/drop-down";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { Button } from "@/components/ui/button" 
 
-const metadata: Metadata = {
-  title: 'Inicio Padre',
-}
-
-export default async function InicioPadrePage() {
+export default function InicioPadrePage() {
   return (
-    <>
-      <InicioPadre/>
-    </>
-  );
-}
-
-function InicioPadre() {
-  const fechaActual = formatearFecha(new Date())
-  return (
-    <div className="h-screen flex flex-col p-5">
-      <div className = "flex flex-row justify-between">
-        <div className = "flex flex-col items-center">
-          <h2 className="text-3xl font-semibold">Título Página</h2>
-          <h2 className="text-md">{fechaActual}</h2>
-        </div>
-        <HijosDropdown/>
-      </div>
-      <div className="flex flex-col mt-auto">
-        <div className = "flex flex-row justify-between">
-          <InfoHijos />
-          <Button variant="outline">Mensajes</Button>
-        </div>
-      </div>
+    <div>
+      
     </div>
-  );
-}
-
-async function InfoHijos(){
-  return(
-  <DropdownMenu>
-  <DropdownMenuTrigger asChild>
-    <Button variant="outline">Información del Hijo/s</Button>
-  </DropdownMenuTrigger>
-  <DropdownMenuContent>
-    <DropdownMenuItem>Amonestaciones</DropdownMenuItem>
-    <DropdownMenuItem>Boletines</DropdownMenuItem>
-    <DropdownMenuItem>Notas Exámenes</DropdownMenuItem>
-    <DropdownMenuItem>Asistencias</DropdownMenuItem>
-    <DropdownMenuItem>Trabajos Practicos</DropdownMenuItem>
-  </DropdownMenuContent>
-</DropdownMenu>
   )
-}
-
-function formatearFecha(fecha : Date){
-  const dia = String(fecha.getDate()).padStart(2, '0'); // Asegura que el día tenga dos dígitos
-  const mes = String(fecha.getMonth() + 1).padStart(2, '0'); // Los meses son 0-11, así que se suma 1
-  const año = fecha.getFullYear();
-  // Construye la fecha en el formato dd/mm/aaaa
-  const fechaFormateada = `${dia}/${mes}/${año}`;
-  return fechaFormateada
 }
