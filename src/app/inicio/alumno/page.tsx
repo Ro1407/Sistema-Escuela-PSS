e75048ab502a91793274595dc1ec5cf4082d19ad
Francisco Schmidt <franschmidt2415@gmail.com>
2024-09-28 17:06:15 -0300

    Corrige inicio alumno y marcas campos obligatorios

--- src/app/inicio/alumno/page.tsx
+++ src/app/inicio/alumno/page.tsx
@@ -1,38 +1,67 @@
-import { Button } from "@/components/ui/button";
-import { Metadata } from "next";
+
+import { Metadata } from "next"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Button } from "@/components/ui/button"
 
 const metadata: Metadata = {
   title: 'Inicio Alumno',
 }
 
 export default async function InicioAlumnoPage() {
-  const nbre = "Lautaro Gonzales"
   return (
     <>
-      <InicioAlumno nombre = {nbre}/>
+      <InicioAlumno />
     </>
   );
 }
 
-async function InicioAlumno({nombre} : {nombre:string}) {
+function InicioAlumno() {
+  const fechaActual = formatearFecha(new Date())
   return (
-    <div className = "p-5">
-      <div className="flex flex-row justify-end">
-        <div className="flex flex-col">
-          {nombre}
-          <Button variant="outline">Alumno</Button>
+    <div className="h-screen flex flex-col p-5">
+      <div className="flex flex-row justify-between">
+        <div className="flex flex-col items-center">
+          <h2 className="text-3xl font-semibold">Título Página</h2>
+          <h2 className="text-md">{fechaActual}</h2>
         </div>
       </div>
-      <div className="flex flex-row">
-        <div className="flex flex-col space-y-2 ml-10">
-          <Button variant="outline">Visualizar Amonestaciones</Button>
-          <Button variant="outline">Boletines</Button>
-          <Button variant="outline">Notas de Examenes</Button>
-          <Button variant="outline">Mensajeria</Button>
-          <Button variant="outline">Visualizar asistencia</Button>
-          <Button variant="outline">Materias</Button>
+      <div className="flex flex-col mt-auto">
+        <div className="flex flex-row justify-between">
+          <InfoEscolar />
+          <Button variant="outline">Mensajes</Button>
         </div>
       </div>
     </div>
+  );
+}
+
+async function InfoEscolar() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline">Información Escolar</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem>Amonestaciones</DropdownMenuItem>
+        <DropdownMenuItem>Boletines</DropdownMenuItem>
+        <DropdownMenuItem>Notas Exámenes</DropdownMenuItem>
+        <DropdownMenuItem>Asistencias</DropdownMenuItem>
+        <DropdownMenuItem>Trabajos Prácticos</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
+}
+
+function formatearFecha(fecha: Date) {
+  const dia = String(fecha.getDate()).padStart(2, '0'); // Asegura que el día tenga dos dígitos
+  const mes = String(fecha.getMonth() + 1).padStart(2, '0'); // Los meses son 0-11, así que se suma 1
+  const año = fecha.getFullYear();
+  // Construye la fecha en el formato dd/mm/aaaa
+  const fechaFormateada = `${dia}/${mes}/${año}`;
+  return fechaFormateada
 }
