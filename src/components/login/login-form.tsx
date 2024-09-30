f818e5d48107c1d00c9c0079cc9fe26e0c3b365b
Ro <rociozentrigen@gmail.com>
2024-09-30 13:49:48 -0300

    Alertas homogeneas

--- src/components/login/login-form.tsx
+++ src/components/login/login-form.tsx
@@ -8,9 +8,8 @@ import {
   ExclamationCircleIcon,
   KeyIcon,
 } from "@heroicons/react/24/outline";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { useFormState, useFormStatus } from "react-dom";
-import { useEffect } from "react";
 import { usePathname } from "next/navigation";
 
 type AuthState = {
@@ -26,6 +25,7 @@ const initialState: AuthState = {
 export default function LoginForm() {
   const [state, dispatch] = useFormState<AuthState, FormData>(validateUser, initialState);
   const pathname = usePathname();
+  const [formError, setFormError] = useState<string | null>(null);
 
   useEffect(() => {
     if (state.redirectPath && pathname !== state.redirectPath) {
@@ -33,14 +33,49 @@ export default function LoginForm() {
     }
   }, [state.redirectPath, pathname]);
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+    const username = form.username as HTMLFormElement;
+    const password = form.password as HTMLFormElement;
+
+    if(!username.value){
+        setFormError('Por favor, ingrese su nombre de usuario');
+        return;
+    }
+    if(!password.value){
+      setFormError('Por favor, ingrese su contraseña');
+      return;
+    }
+
+    setFormError(null);
+    dispatch(formData);
+  }
+
+  function ErrorMessage({ error }: { error: string | null }) {
+    if (!error) return null;
+
+    return (
+        <div
+            className="flex h-8 items-end space-x-1"
+            aria-live="polite"
+            aria-atomic="true"
+        >
+          <ExclamationCircleIcon className="h-5 w-5 text-red-500"/>
+          <p className="text-sm text-red-500">{formError || state.error}</p>
+        </div>
+    );
+  }
+
   return (
-    <div className="flex h-screen w-full items-center justify-center bg-gray-100 px-4 dark:bg-gray-950">
-      <div className="w-full max-w-md rounded-lg bg-white p-6 shadow-lg dark:bg-gray-900">
-        <div className="mb-6 text-center">
+      <div className="flex h-screen w-full items-center justify-center bg-gray-100 px-4 dark:bg-gray-950">
+        <div className="w-full max-w-md rounded-lg bg-white p-6 shadow-lg dark:bg-gray-900">
+          <div className="mb-6 text-center">
           <h1 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-50">Bienvenido</h1>
           <p className="text-gray-500 dark:text-gray-400">Iniciar sesión</p>
         </div>
-        <form action={dispatch} className="space-y-4">
+        <form onSubmit={handleSubmit}  action={dispatch} className="space-y-4">
           <div>
             <label className="mb-2 block text-sm font-medium text-gray-700 dark:text-gray-300" htmlFor="user">
               Usuario
@@ -52,7 +87,6 @@ export default function LoginForm() {
                 type="text"
                 name="username"
                 placeholder="Usuario"
-                required
               />
               <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900"/>
             </div>
@@ -68,25 +102,13 @@ export default function LoginForm() {
                 placeholder="••••••••"
                 type="password"
                 name="password"
-                required
                 minLength={4}
               />
               <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900"/>
             </div>
           </div>
           <LoginButton/>
-          <div
-            className="flex h-8 items-end space-x-1"
-            aria-live="polite"
-            aria-atomic="true"
-          >
-            {state.error && (
-              <>
-                <ExclamationCircleIcon className="h-5 w-5 text-red-500"/>
-                <p className="text-sm text-red-500">{state.error}</p>
-              </>
-            )}
-          </div>
+          <ErrorMessage error={formError || state.error} />
         </form>
       </div>
     </div>
@@ -96,7 +118,7 @@ export default function LoginForm() {
 function LoginButton() {
   const { pending } = useFormStatus();
   return (
-    <Button className="mt-4 w-full" aria-disabled={pending}>
+    <Button className="mt-4 w-full" aria-disabled={pending} type="submit">
       Iniciar sesión
     </Button>
   );
