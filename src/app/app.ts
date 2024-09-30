ec816346f1081675ea8aa040d8f95f547964ed30
Ro <rociozentrigen@gmail.com>
2024-09-29 21:10:15 -0300

    Test de app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,19 @@
 import express from 'express'
-import routes from '../../prisma/routes'
-import errorMiddleware from "../../prisma/error.middleware";
+// import routes from '../../prisma/routes'
+// import errorMiddleware from "../../prisma/error.middleware";
 
 const app = express()
 
 app.use(express.json())
-app.use('/api', routes)
+// app.use('/api', routes)
 
-app.use(errorMiddleware)
+// app.use(errorMiddleware)
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
