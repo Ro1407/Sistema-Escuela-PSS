d19f6da403259e563ebcf426c96a11bad3379331
Ro <rociozentrigen@gmail.com>
2024-10-14 14:45:10 -0300

    Ver si el puerto colisiona

--- src/app/app.ts
+++ src/app/app.ts
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
